f13c287d110f351863294324ca706f1c2a26eaf0
DamienCM <45606435+DamienCM@users.noreply.github.com>
2021-03-01 12:03:23 +0100

    stereo a tester avec des sons qui different vraiment en stereo

--- spectrogram.py
+++ spectrogram.py
@@ -1,19 +1,24 @@
 import soundfile
 import sounddevice as sd
 from numpy.fft import fft
-from numpy import abs,transpose,array,linspace,round,flip
+from numpy import abs,transpose,array,linspace,round,flip,shape,reshape
 import matplotlib.pyplot as plt
 from pydub import AudioSegment 
 import IPython
 
 
-def spectrogramme_wav(file,time_step=0.05,play=False,frequence_enregistrement=None, displayStretch = 10):
+def spectrogramme_wav(file,time_step=0.05,play=False,frequence_enregistrement=None, displayStretch = 10,cmap='Blues',stereo = False):
     #display stretch = coefficient de division de la hauteur max (frequence du graphique)
-    sound = AudioSegment.from_wav(file)
-    sound = sound.set_channels(1)
-    sound.export('temp.wav', format='wav')
+    plt.rcParams["figure.figsize"] = (17,10)
+    if not stereo :
+        sound = AudioSegment.from_wav(file)
+        sound = sound.set_channels(1)
+        sound.export('temp.wav', format='wav')
 
-    son_array,frequence_enr = soundfile.read('temp.wav')
+        son_array,frequence_enr = soundfile.read('temp.wav')
+    else :
+        son_array,frequence_enr = soundfile.read(file)
+    
     if play:
         sd.play(son_array,frequence_enr)
 
@@ -33,26 +38,49 @@ def spectrogramme_wav(file,time_step=0.05,play=False,frequence_enregistrement=No
         current_fft = abs(fft( son_array[int(i*step_index) : int((i+1)*step_index)] ))[0:int(step_index/2)] #on ne garde que la moitie des valeurs (repetition)
         fft_mat.append(current_fft)
 
+    if stereo :
+        s = shape(fft_mat)
+        fft_mat= reshape(fft_mat,(s[-1],s[0],s[1]))
+            
     #Plotting
-    fig,axs = plt.subplots()
    # tmp = transpose (fft_mat)
-    cm = axs.imshow(transpose(fft_mat),cmap = 'Blues')
-    ymax = axs.get_ylim()[0]
-    axs.set_ylim(0,ymax/displayStretch) #empiric for size
-    axs.set_aspect('auto') #Pixels arn't squares anymore
-    xmax,xmin = axs.get_xlim()
-    axs.set_xticks(linspace(xmin,xmax,10))
-    axs.set_xticklabels(flip(linspace(0,T,10).round(2)))
-    cbar = fig.colorbar(cm)
-    cbar.set_label('Amplitude [1]')
-    axs.set_xlabel('Temps [s]')
-    axs.set_ylabel('Fréquence [Hz]') #je suis pas certain de ca 
-    ymin,ymax = axs.get_ylim()
-    axs.set_yticks(linspace(ymin,ymax,10))
-    axs.set_yticklabels((T*linspace(ymin,ymax,10)).round(2))
+    if not stereo:
+        cm = axs.imshow(transpose(fft_mat),cmap = cmap)
+        fig,axs = plt.subplots()
+        ymax = axs.get_ylim()[0]
+        axs.set_ylim(0,ymax/displayStretch) #empiric for size
+        axs.set_aspect('auto') #Pixels arn't squares anymore
+        xmax,xmin = axs.get_xlim()
+        axs.set_xticks(linspace(xmin,xmax,10))
+        axs.set_xticklabels(flip(linspace(0,T,10).round(2)))
+        cbar = fig.colorbar(cm)
+        cbar.set_label('Amplitude [1]')
+        axs.set_xlabel('Temps [s]')
+        axs.set_ylabel('Fréquence [Hz]') #je suis pas certain de ca 
+        ymin,ymax = axs.get_ylim()
+        axs.set_yticks(linspace(ymin,ymax,10))
+        axs.set_yticklabels((T*linspace(ymin,ymax,10)).round(2))
+    else : 
+        fig,axis = plt.subplots(2,1)
+        for i,axs in enumerate(axis) :
+            cm  = axs.imshow(transpose(fft_mat[i]),cmap=cmap)
+            ymax = axs.get_ylim()[0]
+            axs.set_ylim(0,ymax/displayStretch) #empiric for size
+            axs.set_aspect('auto') #Pixels arn't squares anymore
+            xmax,xmin = axs.get_xlim()
+            axs.set_xticks(linspace(xmin,xmax,10))
+            axs.set_xticklabels(flip(linspace(0,T,10).round(2)))
+            axs.set_xlabel('Temps [s]')
+            axs.set_ylabel('Fréquence [Hz]') #je suis pas certain de ca 
+            ymin,ymax = axs.get_ylim()
+            axs.set_yticks(linspace(ymin,ymax,10))
+            axs.set_yticklabels((T*linspace(ymin,ymax,10)).round(2))
+            cbar = fig.colorbar(cm,ax=axs)
+            cbar.set_label('Amplitude [1]')
+
     plt.show()
 
-spectrogramme('faded.wav', displayStretch=20)
+spectrogramme_wav('test.wav', displayStretch=20,time_step=0.05,play=True,stereo=True)
 
 
 IPython.embed()
