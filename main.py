90a7ef2934030e84ebf29faccd3084e62f515aab
DamienCM <damien.cartiermillon@gmail.com>
2021-03-25 09:45:55 +0100

    Ajout d'un TODO au debut du main

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import spectrogram
 from numpy import transpose
 import matplotlib.pyplot as plt
 
+'''
+TODO :
+Il faut trouver pour chaque paire de pixel dans un colomne du spectro la valeur complexe qui une fois IFFT donne un signal reel (ou presque)
 
+    I   ------->    C   ------>   S
+  Float          Complexe       Signal
+  Image          FFT_MAT         Reel
+
+
+Ie : Il faut reussir a trouver une etape qui permet de passer de I a C
+
+'''
 # Test de la fonction inverse avec Fils du Voisin dans le but de le comparer avec le son original
 [fft_mat_output, frequence_enr ] = spectrogram.spectrogramme_wav('audio/filsDuVoisin.wav', displayStretch=1, time_step=.0001, play=False, stereo=False)
 
